41d6d56113e7999a51a2e23a635055d012f30c31
Tian Song <tian.y.song@gmail.com>
2014-05-19 10:43:56 -0700

    Added a new test for passing date

--- test/tests.js
+++ test/tests.js
@@ -3,11 +3,14 @@ var expect = chai.expect;
 var _ = require('../index');
 
 var dulyNoted = function () {
-	console.log('This is the set time to log this information');
-}
+	console.log('Test log');
+};
 
 describe('_.remind()', function() {
 	it('accepts two parameters', function() {
 		expect(_.remind(1,dulyNoted)).to.eql(1,1);
+	});
+	it('accepts a future date', function () {
+		expect(_.remind(new Date(2014, 4, 19, 10, 40, 0), dulyNoted)).to.eql('Mon,'Test log');
 	});
 });
